050484a98b16ff44c2eede3badd3112be1f84b08
Kurt Petrek <kurtpetrek@gmail.com>
2017-10-13 19:22:10 -0400

    starting repo with proof of concept

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Tile1 from './Components/Tile1';
+
+class App extends Component {
+  render() {
+    return (
+      <Tile1/>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Tile2.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class Tile2Container extends Component {
+  costructor(props) {
+    super(props);
+    this.state = {
+      animationPlayState: 'paused'
+    }
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
